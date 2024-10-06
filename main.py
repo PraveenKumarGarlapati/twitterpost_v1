0371ef9ddfc82ea4f673545df6f82b4ec31c1ca6
PraveenGarlapati <prvn012@gmail.com>
2024-10-06 09:43:58 +0530

    Used NewsAPI, refined prompts & using gemini pro APIs

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import os
 import tweepy
 from gnews import GNews
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
+#Getting all the necessary keys in
 api = os.environ['GH_API']
 api_secret = os.environ['GH_API_SECRET'] 
 access_token = os.environ['GH_ACCESS_TOKEN']
 access_token_secret = os.environ['GH_ACCESS_TOKEN_SECRET']
 GEMINI_API_KEY = os.environ['GH_GEMINI_API']
+news_api = os.environ['GH_NEWS_API']
 
 
 ## Gets the latest news from India, pickes the top 2
@@ -21,18 +24,29 @@ google_news.language = 'english'
 news_list = google_news.get_news_by_location('India')
 print(news_list)
 
-titles = [item['title'] for item in news_list]
-titles_string = ','.join(titles[:2])
+titles = [item['description'] for item in news_list]
+titles_string = '\n'.join(titles[:2])
 print(titles_string)
 
 ###############
 ### Passes the above trending news as a prompt to LLM and gets a quirky tweet.
 ## Double check once this is deployed
-
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-response = model.generate_content(f"Give me a quirky tweet along with hashtags. Length has to be less than 160 characters. Here are your cues - {titles_string}..")
+model = genai.GenerativeModel(model_name="gemini-pro")
+
+safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,      
+    }
+
+
+prompt = f"Give me a quirky tweet along with hashtags. Length has to be less than 160 characters. Here are your cues - {titles_string}.."
+response = model.generate_content(
+    prompt, 
+    safety_settings = safety_settings)
 print(response.text)
 
 ###############
