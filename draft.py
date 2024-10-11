2d2a4ba8b6d906677e39c59153aaae0e2cac19fb
PraveenKumarGarlapati <prvn012@gmail.com>
2024-10-11 13:49:09 +0530

    updated source as google-in news

--- draft.py
+++ draft.py
@@ -7,19 +7,22 @@ import random
 from dotenv import load_dotenv
 
 load_dotenv()
-
-news_api = os.environ['GH_NEWS_API']
+news_api = 'ee7fa319f91c4d948fee37f130acc2fe'
+# news_api = os.environ['GH_NEWS_API']
 
 from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key=news_api)
 
 #Pick a topic at random to get the news
-master_list = ['AI','business','finance','sports','politics']
+master_list = ['science','business','technology','sports','entertainment']
 selected_topic = random.choice(master_list)
-selected_topic = 'india'
+# selected_topic = 'india'
 
-# Get the dump of all news items
-all_articles = newsapi.get_everything(q=selected_topic,
-                                      language='en',
-                                      sort_by='relevancy',
-                                      )
+all_articles = newsapi.get_top_headlines(sources = 'google-news-in', language = 'en')
+sources = newsapi.get_sources(country='in')
+
+# # Get the dump of all news items
+# all_articles = newsapi.get_everything(q=selected_topic,
+#                                       language='en',
+#                                       sort_by='relevancy',
+#                                       )
