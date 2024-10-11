d52cb2e992425df8620e706379cb42a2757df751
PraveenKumarGarlapati <prvn012@gmail.com>
2024-10-11 13:55:40 +0530

    update google-in as source

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key=news_api)
 
 #Pick a topic at random to get the news
-# master_list = ['AI','business','finance','sports','politics']
-# selected_topic = random.choice(master_list)
-selected_topic = 'india'
-
+master_list = ['science','business','technology','sports','entertainment']
+selected_topic = random.choice(master_list)
 
 # Get the dump of all news items
-all_articles = newsapi.get_everything(q=selected_topic,
-                                      language='en',
-                                      sort_by='relevancy'
-                                      )
+all_articles = newsapi.get_top_headlines(sources = 'google-news-in', language = 'en')
+
+# all_articles = newsapi.get_everything(q=selected_topic,
+#                                       language='en',
+#                                       sort_by='relevancy'
+#                                       )
 
 
 ### Setup LLM API Key
